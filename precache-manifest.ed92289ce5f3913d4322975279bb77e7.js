35639ac55986aba09299a196e70c1597abdfe350
Th3.C0d3r-418 = Rafael Luis <rafaellfra@hotmail.com>
2020-06-25 15:20:03 -0300

    Deploy via VSCode

---
+++ precache-manifest.ed92289ce5f3913d4322975279bb77e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828773e77d649790c463da52ea4bb5de",
+    "url": "/React-Countries/index.html"
+  },
+  {
+    "revision": "e806627b73c222391699",
+    "url": "/React-Countries/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "49085253ecb402e8c9b5",
+    "url": "/React-Countries/static/css/main.17386685.chunk.css"
+  },
+  {
+    "revision": "e806627b73c222391699",
+    "url": "/React-Countries/static/js/2.c15ca7d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Countries/static/js/2.c15ca7d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49085253ecb402e8c9b5",
+    "url": "/React-Countries/static/js/main.5557b66f.chunk.js"
+  },
+  {
+    "revision": "fc400997171a53c1ce07",
+    "url": "/React-Countries/static/js/runtime-main.5706c700.js"
+  }
+]);
